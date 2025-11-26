94544c844ca6b0970a14a799bf0c54bb29b0cfbc
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-11-25 22:16:45 -0300

    adicionando dinamica no backend

--- RocketSeat/src/server.js
+++ RocketSeat/src/server.js
@@ -1,7 +1,24 @@
 import http from "node:http";
 
-const server = http.createServer((request, response) => {
-  return response.end("Viva ao primeiro servidor");
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/products") {
+    return response.end("viva a lista de produtos");
+  }
+  if (method === "POST" && url === "/products") {
+    const buffer = [];
+
+    for await (const chunk of request) {
+      buffer.push(chunk);
+    }
+    console.log(buffer);
+    console.log(Buffer.concat(buffer).toString());
+    return response
+      .writeHead(201)
+      .end("Viva a esse produto que foi cadastrado");
+  }
+  return response.writeHead(404).end("404 kkk");
 });
 
 server.listen(3333);
