1bf316c1710346edba0be5dc3fb827761804a64d
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-11-25 23:11:57 -0300

    adicionando retorno com body

---
+++ RocketSeat/src/middlewares/jsonBodyHandlder.js
@@ -0,0 +1,18 @@
+export async function jsonBodyHandlder(request, response) {
+  //Adiciona cada chunk
+  const buffers = [];
+
+  //Coleta os chumks de dados da requisição
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+  try {
+    //Concatena os chunks e converter para string. Em seguida, converte a string para JSON.
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+    console.log(request.body);
+  } catch (error) {
+    request.body = null;
+  }
+  //Define o header de resposta como json
+  response.setHeader("Content-Type", "application/json");
+}
--- RocketSeat/src/server.js
+++ RocketSeat/src/server.js
@@ -1,22 +1,16 @@
 import http from "node:http";
+import { jsonBodyHandlder } from "./middlewares/jsonBodyHandlder.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  await jsonBodyHandlder(request, response);
 
   if (method === "GET" && url === "/products") {
     return response.end("viva a lista de produtos");
   }
   if (method === "POST" && url === "/products") {
-    const buffer = [];
-
-    for await (const chunk of request) {
-      buffer.push(chunk);
-    }
-    console.log(buffer);
-    console.log(Buffer.concat(buffer).toString());
-    return response
-      .writeHead(201)
-      .end("Viva a esse produto que foi cadastrado");
+    return response.writeHead(201).end(JSON.stringify(request.body));
   }
   return response.writeHead(404).end("404 kkk");
 });
