6742c67edb607f6bdac1a39468f4a61303745116
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-12-17 23:36:25 -0300

    feat: add course modules endpoints and enforce foreign key constraints in database

--- fullstack-query-builder-template/src/server.ts
+++ fullstack-query-builder-template/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { knex } from "./database/knex";
+import { request } from "http";
 
 const app = express();
 app.use(express.json());
@@ -36,5 +37,32 @@ app.delete("/courses/:id", async (request: Request, response: Response) => {
 
   return response.json();
 });
+app.post("/modules", async (request: Request, response: Response) => {
+  const { name, course_id } = request.body;
+
+  await knex("course_modules").insert({ name, course_id });
+
+  return response.status(201).json();
+});
+app.get("/modules", async (request: Request, response: Response) => {
+  const modules = await knex("Course_modules").select();
+
+  return response.status(201).json(modules);
+});
+
+app.get(
+  "/courses/:id/modules",
+  async (request: Request, response: Response) => {
+    const courses = await knex("courses")
+      .select(
+        "course_modules.id",
+        "course_modules.name AS module",
+        "courses.name AS course"
+      )
+      .join("course_modules", "courses.id", "course_modules.course_id");
+
+    return response.json(courses);
+  }
+);
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
