e801b4e3a66268e4a1bf6b0b8169c9f915a2913d
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-12-28 15:47:29 -0300

    feat: add initial project structure and configuration files for fullstack application

--- fullstack-auth-template/src/controllers/sessions-controller.ts
+++ fullstack-auth-template/src/controllers/sessions-controller.ts
@@ -1,7 +1,29 @@
-import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError";
+import { Request, Response } from "express";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const fakeUser = {
+      id: "1",
+      username: "fabrica",
+      password: "123456",
+      role: "customer",
+    };
+    const { username, password } = request.body;
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("nome de usuário e/ou senha incorreta", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({ role: fakeUser.role }, secret, {
+      expiresIn: expiresIn,
+      subject: String(fakeUser.id),
+    });
+    return response.json({ token });
+  }
 }
 
-export { SessionsController }
+export { SessionsController };
--- fullstack-auth-template/src/routes/products.routes.ts
+++ fullstack-auth-template/src/routes/products.routes.ts
@@ -1,10 +1,20 @@
-import { Router } from "express"
-import { ProductsController } from "@/controllers/products-controller"
+import { Router } from "express";
+import { ProductsController } from "@/controllers/products-controller";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
-const productsRoutes = Router()
-const productsController = new ProductsController()
+const productsRoutes = Router();
+const productsController = new ProductsController();
 
-productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", productsController.create)
+// Adiciona middleware em todas as rotas
+// productsRoutes.use(verifyUserAuthorization(["sale", "customer"]))
 
-export { productsRoutes }
+productsRoutes.get("/", productsController.index);
+productsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "customer"]),
+  productsController.create
+);
+
+export { productsRoutes };
